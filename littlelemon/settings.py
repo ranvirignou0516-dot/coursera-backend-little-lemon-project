feddf81a953771e154a52b89218ad57aece94ffa
Ranvir Kumar <ranvir.ignou0516@gmail.com>
2026-07-29 14:57:58 +0530

    Added Djoser authentication and Menu model unit test

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -38,8 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'restaurant',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
