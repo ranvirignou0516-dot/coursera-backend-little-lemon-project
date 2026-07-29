193f39ab76344fbfe44e049bd531530672052955
Ranvir Kumar <ranvir.ignou0516@gmail.com>
2026-07-29 16:56:29 +0530

    Fixed booking URL routing

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .import views, MenuItemsView, SingleMenuItemView, BookingViewSet
+from . import views
 
 router = DefaultRouter()
-router.register(r'booking',views.BookingViewSet)
+router.register(r'', views.BookingViewSet, basename='booking')
 
 urlpatterns = [
-    path("menu/", views.MenuItemsView.as_view(), name="menu-items"),
-    path("menu/<int:pk>/", views.SingleMenuItemView.as_view(), name="single-menu-item"),
-    path("booking/", include(router.urls)),
+    path('menu/', views.MenuItemsView.as_view(), name='menu-items'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
+
+    path('booking/', include(router.urls)),
 ]
