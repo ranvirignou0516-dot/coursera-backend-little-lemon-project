021d81c35cf1c9ea63a5b447483afd1eea6a6921
Ranvir Kumar <ranvir.ignou0516@gmail.com>
2026-07-29 13:50:45 +0530

    Configured Django REST Framework and Menu API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
-# Create your views here.
+# Menu Items View (GET all items, POST new item)
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+# Single Menu Item View (GET, PUT, DELETE single item by ID)
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
