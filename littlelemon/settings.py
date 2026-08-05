e4c8ceb5d144f458ea747512e520797778170176
ranvirignou0516-dot <ranvir.ignou0516@gmail.com>
2026-08-05 20:25:29 +0530

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'littlelemon',
         'USER': 'littlelemon',
-        'PASSWORD': 'sikkimmca@95',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
